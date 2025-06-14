ffc5b01a11fbfa85a4c7b87e5f4f6b4af7f64e39
Thomas <20701161+thomphil@users.noreply.github.com>
2025-06-14 09:32:59 +0100

    Rename Django project to api

--- service/api/api/settings.py
+++ service/api/api/settings.py
@@ -26,7 +26,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "myproject.urls"
+ROOT_URLCONF = "api.urls"
 
 TEMPLATES = [
     {
@@ -43,7 +43,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "myproject.wsgi.application"
+WSGI_APPLICATION = "api.wsgi.application"
 
 DATABASES = {
     "default": {
